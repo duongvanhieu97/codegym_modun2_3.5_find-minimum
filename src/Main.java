7c1301514d6b37566a4f7d0ce2c0d855c50b788c
Duonghieu97st <Duonghieu97st@gmail.com>
2022-08-05 15:41:13 +0700

    update find minimun

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        int[] arr = {4, 12, 2, 3, 31,3, 6, 29};
+        int index = minValue(arr);
+        System.out.println("Phần tử nhỏ nhất trong mảng: " + arr[index]);
+    }
+
+    public static int minValue(int[] array) {
+        int index = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < array[index]) {
+                index = i;
+            }
+        }
+        return index;
+    }
 }
